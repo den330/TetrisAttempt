c0e49689b250cabaa5f25676b016b7c078107b45
den330 <wo131485508@qq.com>
2024-02-13 14:57:50 -0500

    update

--- index.js
+++ index.js
@@ -59,3 +59,33 @@ const Tetrominoes = [
     [0, 1, 0, 0],
   ]),
 ];
+
+const BoardArray = List.generate(rows, (_) => List.generate(cols, (_) => 0));
+let currentTetromino;
+let nextTeromino = Tetrominoes[Random().nextInt(0, Tetrominoes.length)];
+function updateGame() {
+  if (currentTetromino == null) {
+    currentTetromino = nextTeromino;
+    nextTeromino = Object.assign(
+      {},
+      Tetrominoes[Random().nextInt(0, Tetrominoes.length)]
+    );
+  }
+  if (currentTetromino != null) {
+    currentTetromino.y++;
+  }
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < cols; j++) {
+      const realY = i + currentTetromino.y;
+      if (BoardArray[realY][j] == 1 || realY + 1 >= rows) {
+        currentTetromino = null;
+
+        break;
+      }
+    }
+  }
+}
+
+settimeinterval(() => {
+  updateGame();
+}, 1000);
