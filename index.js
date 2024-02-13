ebf0878eed24b263090e7c37f622697594802d6c
den330 <wo131485508@qq.com>
2024-02-13 14:31:44 -0500

    shapes

--- index.js
+++ index.js
@@ -1,48 +1,61 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-const blockSize = 40;
-const horizontalBlockCount = canvas.width / blockSize;
-const verticalBlockCount = canvas.height / blockSize;
+const canvasHeight = 600;
+const canvasWidth = 300;
+const cellSize = 30;
+const rows = canvasHeight / cellSize;
+const cols = canvasWidth / cellSize;
 
-let currentRow = 0;
-let grid = [];
-while (currentRow < verticalBlockCount) {
-  let rowArr = [];
-  let currentCol = 0;
-  while (currentCol < horizontalBlockCount) {
-    rowArr.push(0);
-    currentCol++;
-  }
-  grid.push(rowArr);
-  currentRow++;
-}
-
-let x = 5;
-let y = 0;
-grid[y][x] = 1;
-drawBoard();
-
-setInterval(() => {
-  if (y < verticalBlockCount - 1) {
-    grid[y][x] = 0;
-    y++;
-    grid[y][x] = 1;
-    drawBoard();
-  }
-}, 800);
-
-function drawBoard() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (let row = 0; row < grid.length; row++) {
-    for (let col = 0; col < grid[row].length; col++) {
-      const x = col * blockSize;
-      const y = row * blockSize;
-      if (grid[row][col] === 1) {
-        ctx.fillStyle = "red";
-        ctx.fillRect(x, y, blockSize, blockSize);
-        ctx.strokeStyle = "yellow";
-        ctx.strokeRect(x, y, blockSize, blockSize);
-      }
-    }
+class Tetromino {
+  x;
+  y;
+  shape;
+  constructor(shape) {
+    this.x = (cols / 2 - 1).truncate();
+    this.y = 0;
+    this.shape = shape;
   }
 }
+
+const Tetrominoes = [
+  Tetromino([
+    [0, 0, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 0],
+  ]),
+  Tetromino([
+    [0, 0, 0, 0],
+    [0, 1, 1, 0],
+    [0, 1, 1, 0],
+    [0, 0, 0, 0],
+  ]),
+  Tetromino([
+    [0, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 1, 1],
+    [0, 0, 0, 0],
+  ]),
+  Tetromino([
+    [0, 0, 0, 0],
+    [0, 0, 0, 1],
+    [0, 1, 1, 1],
+    [0, 0, 0, 0],
+  ]),
+  Tetromino([
+    [0, 0, 0, 0],
+    [0, 1, 1, 0],
+    [0, 0, 1, 1],
+    [0, 0, 0, 0],
+  ]),
+  Tetromino([
+    [0, 0, 0, 0],
+    [0, 0, 1, 1],
+    [0, 1, 1, 0],
+    [0, 0, 0, 0],
+  ]),
+  Tetromino([
+    [0, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 1, 0],
+    [0, 1, 0, 0],
+  ]),
+];
